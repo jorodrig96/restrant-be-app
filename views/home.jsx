82cbb2a72a96f83ac6e8ac692653e9dc07d0d62c
Jose Rodriguez <joserodriguez@Joses-MacBook-Pro-2.local>
2023-08-07 21:31:28 -0700

    added a static css folder

--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,9 @@ function home () {
             <main>
                 <h1>Home page </h1>
                 <p>This page imports from /views/home.jsx. Uses the html from the default.jsx file</p>
+                <a href= "/places">
+                    <button className= "btn-primary">Places Page. Imports from /views / places / indexjsx. </button>
+                </a>
             </main>
         </Def>
     );
